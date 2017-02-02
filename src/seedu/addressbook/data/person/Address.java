c149cc16d8b91361ab3ac934beea177bfe6fd83d
Shao Hui <shinlos@hotmail.com>
2017-02-02 15:03:22 +0800

    added address component's classes

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -10,10 +10,14 @@ public class Address {
 
     public static final String EXAMPLE = "123, some street";
     public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
-    public static final String ADDRESS_VALIDATION_REGEX = ".+";
+    public static final String ADDRESS_VALIDATION_REGEX = ".+, .+, .+, .+";
 
-    public final String value;
     private boolean isPrivate;
+    
+    private BlockNum blockNum;
+    private StreetNum streetNum;
+    private UnitNum unitNum;
+    private PostalCode postalCode;
 
     /**
      * Validates given address.
@@ -26,7 +30,11 @@ public class Address {
         if (!isValidAddress(trimmedAddress)) {
             throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
         }
-        this.value = trimmedAddress;
+        String[] addressArr = trimmedAddress.split(",");
+        blockNum = new BlockNum(addressArr[0]);
+        streetNum = new StreetNum(addressArr[1]);
+        unitNum = new UnitNum(addressArr[2]);
+        postalCode = new PostalCode(addressArr[3]);
     }
 
     /**
@@ -38,14 +46,15 @@ public class Address {
 
     @Override
     public String toString() {
-        return value;
+        return blockNum.getBlockNum() + ", " + streetNum.getStreetNum() + ", " + 
+        		unitNum.getUnitNum() + ", " + postalCode.getPostalCode();
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof Address // instanceof handles nulls
-                && this.value.equals(((Address) other).value)); // state check
+                && this.toString().equals(((Address) other).toString())); // state check
     }
 
     @Override
@@ -55,5 +64,53 @@ public class Address {
 
     public boolean isPrivate() {
         return isPrivate;
+    }
+    
+    private class BlockNum {
+    	String blockNum;
+    	public BlockNum(String blockNum) {
+    		this.blockNum = blockNum;
+    	}
+    	
+    	//accessor
+    	public String getBlockNum() {
+    		return blockNum;
+    	}
+    }
+    
+    private class StreetNum {
+    	String streetNum;
+    	public StreetNum(String streetNum) {
+    		this.streetNum = streetNum;
+    	}
+    	
+    	//accessor
+    	public String getStreetNum() {
+    		return streetNum;
+    	}
+    }
+    
+    private class UnitNum {
+    	String unitNum;
+    	public UnitNum(String unitNum) {
+    		this.unitNum = unitNum;
+    	}
+    	
+    	//accessor
+    	public String getUnitNum() {
+    		return unitNum;
+    	}
+    }
+    
+    private class PostalCode {
+    	String postalCode;
+    	public PostalCode(String postalCode) {
+    		this.postalCode = postalCode;
+    	}
+    	
+    	//accessor
+    	public String getPostalCode() {
+    		return postalCode;
+    	}
     }
 }
