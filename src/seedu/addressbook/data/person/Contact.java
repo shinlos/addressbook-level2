b62f8b528d743323a4cbdac1ba83b2baa5c5130b
Shao Hui <shinlos@hotmail.com>
2017-02-08 19:35:59 +0800

    add value trimming and IllegalValueException

--- src/seedu/addressbook/data/person/Contact.java
+++ src/seedu/addressbook/data/person/Contact.java
@@ -1,12 +1,18 @@
 package seedu.addressbook.data.person;
 
+import seedu.addressbook.data.exception.IllegalValueException;
+
 public class Contact {
 	public final String value;
 	private boolean isPrivate;
 	
-	public Contact(String value, boolean isPrivate) {
+	public Contact(String value, boolean isPrivate, String regex, String constraintsMessage) throws IllegalValueException {
 		this.isPrivate = isPrivate;
-		this.value = value;
+		String trimmedValue = value.trim();
+        if (!isValid(trimmedValue, regex)) {
+            throw new IllegalValueException(constraintsMessage);
+        }
+        this.value = trimmedValue;
 	}
 	
 	public String toString() {
@@ -19,5 +25,9 @@ public class Contact {
 
 	public boolean isPrivate() {
         return isPrivate;
+    }
+	
+	public static boolean isValid(String test, String regex) {
+        return test.matches(regex);
     }
 }
