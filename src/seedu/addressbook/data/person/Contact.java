380c1d97cfc82d00cd2262c60b905ae7b4210d7c
Shao Hui <shinlos@hotmail.com>
2017-02-08 12:15:26 +0800

    add constructor

--- src/seedu/addressbook/data/person/Contact.java
+++ src/seedu/addressbook/data/person/Contact.java
@@ -1,7 +1,13 @@
 package seedu.addressbook.data.person;
 
 public class Contact {
+	public final String value;
 	private boolean isPrivate;
+	
+	public Contact(String value, boolean isPrivate) {
+		this.isPrivate = isPrivate;
+		this.value = value;
+	}
 
 	public boolean isPrivate() {
         return isPrivate;
