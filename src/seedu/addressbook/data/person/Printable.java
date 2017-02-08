b419fc89c11412a9ec80ba15bd1e46ec49b61c11
Shao Hui <shinlos@hotmail.com>
2017-02-08 20:41:31 +0800

    add Printable interface

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,8 @@
+package seedu.addressbook.data.person;
+
+public interface Printable {
+	/*
+	 * Gets the string representation for a component of Person's contact information
+	 */
+	public String getPrintableString();
+}
